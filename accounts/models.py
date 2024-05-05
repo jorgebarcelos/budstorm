b1a526dfc0ba1b78035adfc157166aa1d2a4eed9
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2024-05-05 20:18:23 -0300

    :feat: accouts permission models

--- accounts/models.py
+++ accounts/models.py
@@ -21,6 +21,23 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+    
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+
+        return user
 
 
 
@@ -28,7 +45,7 @@ class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
     phone_number = models.CharField(max_length=50)
 
     # required
@@ -41,6 +58,8 @@ class Account(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = MyAccountManager()
 
 
     def __str__(self):
--- config/settings.py
+++ config/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
